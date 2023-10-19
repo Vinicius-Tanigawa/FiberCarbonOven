b4d7ed3681be31e7fd592a444d0d69db41bdbbaa
HonkyJr <vktanigawa@gmail.com>
2023-10-19 15:17:33 -0300

    New code for heat_tranfer

---
+++ heat_transfer.py
@@ -0,0 +1,84 @@
+#heat_tranfer.py
+
+import numpy as np
+
+rc  = (29+.75)/2 /1000 # m - raio externo do carbono
+rpi = .250 # m - raio interno do pvc
+rpe = rpi+.003 # m - raio externo do pvc
+L   = 1.5 # m - comprimento do tubo
+
+P = 5 # W - potência esquentando o carbono
+Tinf = 20 # °C - temperatura ambiente
+
+ka = .03 # W/mK - condutividade térmica do ar
+kp = .2 # W/mK - condutividade térmica do pvc
+h = 1 # W/m²K - coeficiente de convecção do pvc com o ar externo
+
+sigma = 5.67e-8 # W/m²K⁴ - constante de Stefan-Boltzmann
+
+dra = .001 # m - espaçamento dos pontos no ar interno
+drp = .0001 # m - espaçamento dos pontos no pvc
+
+# Variáveis auxiliares
+Na = int(np.round((rpi-rc )/dra))
+Np = int(np.round((rpe-rpi)/drp))
+N = Na+Np+1
+print(Na)
+print(Np)
+print(N)
+
+Ac  = 2*np.pi*rc*L
+Api = 2*np.pi*rpi*L
+
+Tc = Tinf
+Tp = Tinf
+PR = 0
+
+# A*T = B
+T = np.zeros((N))
+A = np.zeros((N,N))
+B = np.zeros((N))
+
+Tc_ant = Tc+1
+
+cont = 0
+while abs(Tc-Tc_ant)>1e-3 and cont<100:
+
+  # Calcula a matriz A e o vetor B
+  for i in range(N):
+    if i==0:
+      A[i][i  ] = 1
+      A[i][i+1] = -1
+      B[i] = dra/Ac/ka*(P-PR)
+    elif 0<i<Na:
+      r = rc + dra*i
+      A[i][i-1] = 2*r - dra
+      A[i][i  ] = -4*r
+      A[i][i+1] = 2*r + dra
+      B[i] = 0
+    elif i==Na:
+      A[i][i-1] = ka/dra
+      A[i][i  ] = -ka/dra-kp/drp
+      A[i][i+1] = kp/drp
+      B[i] = -PR/Api
+    elif Na<i<N-1:
+      r = rpi + drp*(i-Na+1)
+      A[i][i-1] = 2*r - drp
+      A[i][i  ] = -4*r
+      A[i][i+1] = 2*r + drp
+      B[i] = 0
+    elif i==N-1:
+      A[i][i-1] = -1
+      A[i][i  ] = h*drp/kp + 1
+      B[i] = h*drp/kp * Tinf
+  # Resolve o sistema de equações
+  T = np.linalg.solve(A, B)
+
+  Tc = T[0]
+  Tp = T[Na-1]
+  PR = sigma*(Ac*Tc**4 - Api*Tp**4)
+
+  cont+=1
+
+print("P  = "+str(P)+" W")
+print("PR = "+str(PR)+" W")
---
+++ plot.py
@@ -0,0 +1,32 @@
+#plot.py
+
+import plotly.graph_objects as go
+
+from plotly.subplots import make_subplots
+from heat_transfer import *
+
+rs = [ rc + min(i,Na)*dra + max(0,i-Na)*drp for i in range(N) ]
+
+fig = go.Figure()
+
+fig.add_trace(go.Scatter(
+  x=rs,y=T,
+  mode="lines",
+  showlegend = False,
+))
+
+fig.update_layout(
+  title="Distribuição de temperatura no forninho",
+  xaxis = dict(
+    title= "Posição radial [m]",
+    # gridcolor = "lightgrey",
+  ),
+  yaxis = dict(
+    title="Temperatura [°C]",
+    # gridcolor = "lightgrey"
+  ),
+  plot_bgcolor="White",
+  paper_bgcolor="White",
+  template = "plotly_white",
+  width=800,height=400,
+)
